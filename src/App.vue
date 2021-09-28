0bcb2bb33c03b20dedca53e667b1264cf26fe915
cvillier <corentin.villiermet@gmail.com>
2021-09-28 21:21:16 +0200

    improve Accueil.vue

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Accueil/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Accueil from './components/Accueil';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Accueil,
   },
 
   data: () => ({
