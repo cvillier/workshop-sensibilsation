be74333d1a83b05e9ab4ccef5ce654361afdddd3
Jerome Arrigoni <jeromearrigonidev@gmail.com>
2021-09-28 16:24:28 +0200

    DEV: Commit après effacement tragique page accueil

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  store: store,
   render: h => h(App)
 }).$mount('#app')
+
+const store = new Vuex.Store({
+
+  state: {
+    userChoice: [],
+    questionsList: [] // Tableau d'objets
+  },
+
+  getters: {
+    getUserChoice(state) {
+      return state.userChoice
+    }
+  },
+
+  mutations: {
+    setUserChoice(state, payload) {
+      state.userChoice.push(payload)
+    }
+  },
+  actions: {
+    registerUserChoice({ commit }, payload) {
+      commit('setUserChoice', payload)
+    }
+  }
+})
