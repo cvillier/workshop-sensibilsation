e26499c992d0873385687f3bd76e53e7ce1dff86
Jerome Arrigoni <jeromearrigonidev@gmail.com>
2021-09-29 16:38:10 +0200

    Ajout composant question

---
+++ src/components/Question.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-dialog
+      :value="modaleQuestionIsOpen"
+      fullscreen
+      transition="dialog-bottom-transition"
+    >
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  props: ["modaleQuestionIsOpen"],
+
+  data() {
+    return {};
+  },
+};
+</script>
